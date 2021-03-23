c2cf997b4e4fc733f6fb1470d4d453067c9f994f
Merrick McDougal <Merrick@mrmcdougal.com>
2021-03-23 17:19:39 -0500

    Finished Project

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Merrick Mcdougal on 3/20/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+    
+}
